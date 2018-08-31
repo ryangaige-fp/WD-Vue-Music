2fbdbd5d9db3ff6cb0edd6dd320d94807785577d
Ryan Gaige <Ryan@fencepencil.com>
2018-08-30 18:33:24 -0600

    commit

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCXppcpL5EwqlyBne0xOGjNOODlC-QKyV4",
+  authDomain: "firemusic-33879.firebaseapp.com",
+  databaseURL: "https://firemusic-33879.firebaseio.com",
+  projectId: "firemusic-33879",
+  storageBucket: "",
+  messagingSenderId: "17386166295"
+};
